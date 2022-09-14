d77f7c49d0c51f5096e0dc68a722efe973c0fb1b
Pradip Sapkota <pradip.sapkota@fusemachines.com>
2022-09-14 17:47:50 +0545

    practice.py updated

--- practice.py
+++ practice.py
@@ -1,36 +1,24 @@
 import json
-
-def question3():
-    f= open('products.json','r')
+from random import randint
+def adduser():
+    f= open('users.json','r')
     data = json.loads(f.read())
     f.close()
     nd = {
-            "id": 31,
-            "title": "iPhone 9",
-            "description": "An apple mobile which is nothing like apple",
-            "price": 549,
-            "discountPercentage": 12.96,
-            "rating": 4.69,
-            "stock": 94,
-            "brand": "Apple",
-            "category": "Smartphones",
-            "thumbnail": "https://dummyjson.com/image/i/products/1/thumbnail.jpg",
-            "images": [
-                "https://dummyjson.com/image/i/products/1/1.jpg",
-                "https://dummyjson.com/image/i/products/1/2.jpg",
-                "https://dummyjson.com/image/i/products/1/3.jpg",
-                "https://dummyjson.com/image/i/products/1/4.jpg",
-                "https://dummyjson.com/image/i/products/1/thumbnail.jpg"
-            ]
-        }
-    data['products'].append(nd)
-    # with open ('products.json','w') as e:
-    #         e.write(json.dumps(data))
-    returun  (f"<p>{"status":"success"}</p><pre>{json.dumps(data['products'][-1],indent=4)}</pre>")
-question3()
+        "id": data[-1]['id']+1,
+        "name": data[randint(0,len(data)-1)]['name'],
+        "username": data[randint(0,len(data)-1)]['username'],
+        "email": data[randint(0,len(data)-1)]['email'],
+        "address": data[randint(0,len(data)-1)]['address'],
+        "phone": data[randint(0,len(data)-1)]['phone'],
+        "website": data[randint(0,len(data)-1)]['website'],
+        "company": data[randint(0,len(data)-1)]['company']
+    }
+    data.append(nd)
+    with open ('products.json','w') as e:
+            e.write(json.dumps(data))
+    status = {'status':'success','message': 'User added'}
+    return f'<pre>{json.dumps(status,indent=4)}</pre><pre>{json.dumps(nd,indent=4)}</pre>'
 
-# original_dict = {'jack': 38, 'michael': 48, 'guido': 57, 'john': 33}
-# it1 = {k:v for k,v in original_dict.items() if  k=='john'}
-# it2 = {k:v for k,v in original_dict.items() if k=='jack'}
-# print(json.dumps(original_dict,indent=4))
-# print(json.dumps(it1.update(it2),indent=4))
+
+adduser()
